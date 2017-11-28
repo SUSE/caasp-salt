4b4391f3048c7509323aa241b954c83e2a00deda
Alvaro Saurin <alvaro.saurin@gmail.com>
2017-11-28 13:14:41 +0100

    Use a bath size for etcd setup equal to the number of etcd masters (bsc#1066695) Minor cleanups and a fix for a case where caasp_etcd.py could return 0.

--- salt/_modules/caasp_filters.py
+++ salt/_modules/caasp_filters.py
@@ -6,6 +6,8 @@ def is_ip(ip):
     '''
     Returns a bool telling if the passed IP is a valid IPv4 or IPv6 address.
     '''
+    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#is-ip)
+    #       once we Salt>2017.7.0
     return is_ipv4(ip) or is_ipv6(ip)
 
 
@@ -13,6 +15,8 @@ def is_ipv4(ip):
     '''
     Returns a bool telling if the value passed to it was a valid IPv4 address
     '''
+    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#is-ipv4)
+    #       once we Salt>2017.7.0
     try:
         return ipaddress.ip_address(ip).version == 4
     except ValueError:
@@ -23,7 +27,15 @@ def is_ipv6(ip):
     '''
     Returns a bool telling if the value passed to it was a valid IPv6 address
     '''
+    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#is-ipv6)
+    #       once we Salt>2017.7.0
     try:
         return ipaddress.ip_address(ip).version == 6
     except ValueError:
         return False
+
+
+def get_max(seq):
+    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#max)
+    #       once we Salt>2017.7.0
+    return max(seq)
