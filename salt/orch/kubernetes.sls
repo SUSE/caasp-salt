parlt91/salt

Use CNI for the network fabric

salt/orch/kubernetes.sls
@@ -90,6 +90,7 @@ etcd_discovery_setup:
   salt.state:
     - tgt: 'roles:kube-master'
     - tgt_type: grain
+    - batch: 1
     - sls:
       - etcd-discovery
     - require:
@@ -110,16 +111,6 @@ etcd_setup:
     - require:
       - salt: etcd_discovery_setup
 
-flannel_setup:
-  salt.state:
-    - tgt: 'roles:kube-master'
-    - tgt_type: grain
-    - batch: 5
-    - sls:
-      - flannel-setup
-    - require:
-      - salt: etcd_setup
-
 admin_setup:
   salt.state:
     - tgt: 'roles:admin'
@@ -127,7 +118,7 @@ admin_setup:
     - highstate: True
     - batch: 5
     - require:
-      - salt: flannel_setup
+      - salt: etcd_setup
 
 kube_master_setup:
   salt.state:
@@ -147,7 +138,6 @@ kube_minion_setup:
     - highstate: True
     - batch: 5
     - require:
-      - salt: flannel_setup
       - salt: kube_master_setup
 
 reboot_setup:
@@ -159,6 +149,7 @@ reboot_setup:
       - reboot
     - require:
       - salt: kube_master_setup
+      - salt: kube_minion_setup
 
 dex_setup:
   salt.state:
@@ -192,3 +183,16 @@ clear_bootstrap_in_progress_flag:
       - false
     - require:
       - salt: set_bootstrap_complete_flag
+
+# apply manifests, something that could be done even
+# if the cluster was being updated...
+apply_manifests:
+  salt.state:
+    - tgt: 'roles:kube-master'
+    - tgt_type: grain
+    - batch: 1
+    - sls:
+      - addons
+      - cni
+    - require:
+      - salt: clear_bootstrap_in_progress_flag

SUSE/caasp-salt

salt/orch/kubernetes.sls
@@ -116,14 +116,6 @@ etcd_setup:
     - require:
       - salt: etcd_discovery_setup
 
-flannel_setup:
-  salt.state:
-    - tgt: {{ super_master }}
-    - sls:
-      - flannel-setup
-    - require:
-      - salt: etcd_setup
-
 admin_setup:
   salt.state:
     - tgt: 'roles:admin'
@@ -131,7 +123,7 @@ admin_setup:
     - highstate: True
     - batch: {{ default_batch }}
     - require:
-      - salt: flannel_setup
+      - salt: etcd_setup
 
 kube_master_setup:
   salt.state:
@@ -151,8 +143,18 @@ kube_minion_setup:
     - highstate: True
     - batch: {{ default_batch }}
     - require:
-      - salt: flannel_setup
       - salt: kube_master_setup
+
+# we must start CNI right after the masters/minions reach highstate,
+# as nodes will be NotReady until the CNI DaemonSet is loaded and running...
+cni_setup:
+  salt.state:
+    - tgt: {{ super_master }}
+    - sls:
+      - cni
+    - require:
+      - salt: kube_master_setup
+      - salt: kube_minion_setup
 
 reboot_setup:
   salt.state:
@@ -160,7 +162,7 @@ reboot_setup:
     - sls:
       - reboot
     - require:
-      - salt: kube_master_setup
+      - salt: cni_setup
 
 services_setup:
   salt.state: