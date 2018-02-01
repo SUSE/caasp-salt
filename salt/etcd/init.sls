mrostecki/salt

No longer use machine-id's as node names
With CaaSP 3.0, we're introducing a requirement for machines to
have valid+unique hostnames in order to allow for the K8S CPIs
to function correctly.

This means our generated hostname is no longer needed, as our
environment requirements force operators to provision servers
with unique hostnames.

salt/etcd/init.sls
@@ -58,7 +58,7 @@ etcd:
         etcdctl --key-file {{ pillar['ssl']['key_file'] }} \
                 --cert-file {{ pillar['ssl']['crt_file'] }} \
                 --ca-file {{ pillar['ssl']['ca_file'] }} \
-                --endpoints https://{{ grains['caasp_fqdn'] }}:2379 \
+                --endpoints https://{{ grains['host'] }}:2379 \
                 cluster-health | grep "cluster is healthy"
     - retry:
         attempts: 10

salt/kubelet/init.sls
@@ -19,7 +19,7 @@ include:
 {% endif %}
 
 {% from '_macros/certs.jinja' import certs with context %}
-{{ certs('node:' + grains['caasp_fqdn'],
+{{ certs('node:' + grains['host'],
          pillar['ssl']['kubelet_crt'],
          pillar['ssl']['kubelet_key'],
          o = 'system:nodes') }}
@@ -101,12 +101,12 @@ kubelet:
 {% if not "kube-master" in salt['grains.get']('roles', []) and salt['grains.get']('kubelet:should_uncordon', false) %}
   caasp_cmd.run:
     - name: |
-        kubectl uncordon {{ grains['caasp_fqdn'] }}
+        kubectl uncordon {{ grains['host'] }}
     - retry:
         attempts: 10
         interval: 3
         until: |
-          test "$(kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} get nodes {{ grains['caasp_fqdn'] }} -o=jsonpath='{.spec.unschedulable}' 2>/dev/null)" != "true"
+          test "$(kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} get nodes {{ grains['host'] }} -o=jsonpath='{.spec.unschedulable}' 2>/dev/null)" != "true"
     - require:
       - file: {{ pillar['paths']['kubeconfig'] }}
 {% endif %}

salt/kubelet/stop.sls
@@ -4,13 +4,13 @@
 include:
   - kubectl-config
 
-{% set should_uncordon = salt['cmd.run']("kubectl --kubeconfig=" + pillar['paths']['kubeconfig'] + " get nodes " + grains['caasp_fqdn'] + " -o=jsonpath='{.spec.unschedulable}' 2>/dev/null") != "true" %}
+{% set should_uncordon = salt['cmd.run']("kubectl --kubeconfig=" + pillar['paths']['kubeconfig'] + " get nodes " + grains['host'] + " -o=jsonpath='{.spec.unschedulable}' 2>/dev/null") != "true" %}
 
 # If this fails we should ignore it and proceed anyway as Kubernetes will recover
 drain-kubelet:
   cmd.run:
     - name: |
-        kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} drain {{ grains['caasp_fqdn'] }} --ignore-daemonsets --grace-period=300 --timeout=340s
+        kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} drain {{ grains['host'] }} --ignore-daemonsets --grace-period=300 --timeout=340s
     - check_cmd:
       - /bin/true
     - require:

salt/ldap/init.sls
@@ -5,10 +5,10 @@ include:
 {% set names = [salt.caasp_pillar.get('dashboard')] %}
 
 {% from '_macros/certs.jinja' import alt_names, certs with context %}
-{{ certs("ldap:" + grains['caasp_fqdn'],
+{{ certs("ldap:" + grains['host'],
          pillar['ssl']['ldap_crt'],
          pillar['ssl']['ldap_key'],
-         cn = grains['caasp_fqdn'],
+         cn = grains['host'],
          extra_alt_names = alt_names(names)) }}
 
 openldap_restart:

salt/velum/init.sls
@@ -7,8 +7,8 @@ include:
                 salt.caasp_pillar.get('dashboard')] %}
 
 {% from '_macros/certs.jinja' import alt_names, certs with context %}
-{{ certs("velum:" + grains['caasp_fqdn'],
+{{ certs("velum:" + grains['host'],
          pillar['ssl']['velum_crt'],
          pillar['ssl']['velum_key'],
-         cn = grains['caasp_fqdn'],
+         cn = grains['host'],
          extra_alt_names = alt_names(names)) }}

SUSE/caasp-salt

salt/etcd/init.sls
@@ -58,7 +58,7 @@ etcd:
         etcdctl --key-file {{ pillar['ssl']['key_file'] }} \
                 --cert-file {{ pillar['ssl']['crt_file'] }} \
                 --ca-file {{ pillar['ssl']['ca_file'] }} \
-                --endpoints https://{{ grains['caasp_fqdn'] }}:2379 \
+                --endpoints https://{{ grains['nodename'] }}:2379 \
                 cluster-health | grep "cluster is healthy"
     - retry:
         attempts: 10

salt/kubelet/init.sls
@@ -19,7 +19,7 @@ include:
 {% endif %}
 
 {% from '_macros/certs.jinja' import certs with context %}
-{{ certs('node:' + grains['caasp_fqdn'],
+{{ certs('node:' + grains['nodename'],
          pillar['ssl']['kubelet_crt'],
          pillar['ssl']['kubelet_key'],
          o = 'system:nodes') }}
@@ -101,12 +101,12 @@ kubelet:
 {% if not "kube-master" in salt['grains.get']('roles', []) and salt['grains.get']('kubelet:should_uncordon', false) %}
   caasp_cmd.run:
     - name: |
-        kubectl uncordon {{ grains['caasp_fqdn'] }}
+        kubectl uncordon {{ grains['nodename'] }}
     - retry:
         attempts: 10
         interval: 3
         until: |
-          test "$(kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} get nodes {{ grains['caasp_fqdn'] }} -o=jsonpath='{.spec.unschedulable}' 2>/dev/null)" != "true"
+          test "$(kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} get nodes {{ grains['nodename'] }} -o=jsonpath='{.spec.unschedulable}' 2>/dev/null)" != "true"
     - require:
       - file: {{ pillar['paths']['kubeconfig'] }}
 {% endif %}

salt/kubelet/stop.sls
@@ -4,13 +4,13 @@
 include:
   - kubectl-config
 
-{% set should_uncordon = salt['cmd.run']("kubectl --kubeconfig=" + pillar['paths']['kubeconfig'] + " get nodes " + grains['caasp_fqdn'] + " -o=jsonpath='{.spec.unschedulable}' 2>/dev/null") != "true" %}
+{% set should_uncordon = salt['cmd.run']("kubectl --kubeconfig=" + pillar['paths']['kubeconfig'] + " get nodes " + grains['nodename'] + " -o=jsonpath='{.spec.unschedulable}' 2>/dev/null") != "true" %}
 
 # If this fails we should ignore it and proceed anyway as Kubernetes will recover
 drain-kubelet:
   cmd.run:
     - name: |
-        kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} drain {{ grains['caasp_fqdn'] }} --ignore-daemonsets --grace-period=300 --timeout=340s
+        kubectl --kubeconfig={{ pillar['paths']['kubeconfig'] }} drain {{ grains['nodename'] }} --ignore-daemonsets --grace-period=300 --timeout=340s
     - check_cmd:
       - /bin/true
     - require:

salt/ldap/init.sls
@@ -5,10 +5,10 @@ include:
 {% set names = [salt.caasp_pillar.get('dashboard')] %}
 
 {% from '_macros/certs.jinja' import alt_names, certs with context %}
-{{ certs("ldap:" + grains['caasp_fqdn'],
+{{ certs("ldap:" + grains['nodename'],
          pillar['ssl']['ldap_crt'],
          pillar['ssl']['ldap_key'],
-         cn = grains['caasp_fqdn'],
+         cn = grains['nodename'],
          extra_alt_names = alt_names(names)) }}
 
 openldap_restart:

salt/velum/init.sls
@@ -7,8 +7,8 @@ include:
                 salt.caasp_pillar.get('dashboard')] %}
 
 {% from '_macros/certs.jinja' import alt_names, certs with context %}
-{{ certs("velum:" + grains['caasp_fqdn'],
+{{ certs("velum:" + grains['nodename'],
          pillar['ssl']['velum_crt'],
          pillar['ssl']['velum_key'],
-         cn = grains['caasp_fqdn'],
+         cn = grains['nodename'],
          extra_alt_names = alt_names(names)) }}