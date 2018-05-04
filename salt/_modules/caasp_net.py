c55e86c86a3873e0ca10ac61e7206f526aa9bf3c
Alvaro Saurin <alvaro.saurin@gmail.com>
2018-05-04 18:49:50 +0200

    Try to resist existent data in the mine
    https://bugzilla.suse.com/show_bug.cgi?id=1091077 (bsc#1091077)
    
    See also https://bugzilla.suse.com/show_bug.cgi?id=1091361 (bsc#1091361)
    
    (cherry picked from commit 8388498b542f6c525f17e849c0bbcd6841058acd)

--- salt/_modules/caasp_net.py
+++ salt/_modules/caasp_net.py
@@ -6,51 +6,63 @@
 from __future__ import absolute_import
 
 
+# note: do not import caasp modules other than caasp_log
+from caasp_log import error
+
+
+DEFAULT_INTERFACE = 'eth0'
+
+# DEPRECATED: this will be removed in CaaSP 3.0
+GRAIN_NODENAME = 'caasp_fqdn'
+
+
 def __virtual__():
     return "caasp_net"
 
 
-def _get_mine(*args, **kwargs):
-    return __salt__['mine.get'](*args, **kwargs)
-
-
-def get_iface_ip(iface, **kwargs):
+def get_iface_ip(iface, host=None, ifaces=None):
     '''
     given an 'iface' (and an optional 'host' and list of 'ifaces'),
     return the IP address associated with 'iface'
     '''
-    host = kwargs.pop('host', None)
-    if not host:
-        ifaces = __salt__['network.interfaces']()
-    else:
-        ifaces = kwargs.pop('ifaces', _get_mine(host, 'network.interfaces')[host])
+    try:
+        if not ifaces:
+            if not host or host == get_nodename():
+                ifaces = __salt__['network.interfaces']()
+            else:
+                ifaces = __salt__['caasp_grains.get'](host, 'network.interfaces', type='glob')
 
-    iface = ifaces.get(iface)
-    ipv4addr = iface.get('inet', [{}])
-    return ipv4addr[0].get('address')
+        iface = ifaces.get(iface)
+        ipv4addr = iface.get('inet', [{}])
+        return ipv4addr[0].get('address')
+    except Exception as e:
+        error('could not get IP for interface %s: %s', iface, e)
+        return ''
 
 
-def get_primary_iface(**kwargs):
+def get_primary_iface(host=None):
     '''
     (given some optional 'host')
     return the name of the primary iface (the iface associated with the default route)
     '''
-    host = kwargs.pop('host', None)
-    if not host:
-        default_route_lst = __salt__['network.default_route']()
-        iface = default_route_lst[0]['interface']
-    else:
-        iface = _get_mine(host, 'network.default_route')[host][0]['interface']
+    try:
+        if not host or host == get_nodename():
+            default_route_lst = __salt__['network.default_route']()
+            return default_route_lst[0]['interface']
+        else:
+            all_routes = __salt__['caasp_grains.get'](host, 'network.default_route', type='glob')
+            return all_routes[host][0]['interface']
+    except Exception as e:
+        error('could not get the primary interface: %s', e)
+        return __salt__['pillar.get']('hw:netiface', DEFAULT_INTERFACE)
 
-    return iface
 
-
-def get_primary_ip(**kwargs):
+def get_primary_ip(host=None, ifaces=None):
     '''
     (given an optional minion 'host' and a list of its network interfaces, 'ifaces'),
     return the primary IP
     '''
-    return get_iface_ip(get_primary_iface(**kwargs), **kwargs)
+    return get_iface_ip(iface=get_primary_iface(host=host), host=host, ifaces=ifaces)
 
 
 def get_primary_ips_for(compound, **kwargs):
@@ -58,7 +70,26 @@ def get_primary_ips_for(compound, **kwargs):
     given a compound expression 'compound', return the primary IPs for all the
     nodes that match that expression
     '''
-    res = []
-    for host in _get_mine(compound, 'network.interfaces', expr_form='compound').keys():
-        res.append(get_primary_ip(host=host, **kwargs))
-    return res
+    try:
+        all_ifaces = __salt__['caasp_grains.get'](compound, 'network.interfaces')
+        return [get_primary_ip(host=host, **kwargs) for host in all_ifaces.keys()]
+    except Exception as e:
+        error('could not get primary IPs for %s: %s', compound, e)
+        return []
+
+
+def get_nodename(host=None, **kwargs):
+    '''
+    (given some optional 'host')
+    return the `nodename`
+    '''
+    try:
+        if not host:
+            assert __opts__['__role'] != 'master'
+            return __salt__['grains.get'](GRAIN_NODENAME)
+        else:
+            all_nodenames = __salt__['caasp_grains.get'](host, GRAIN_NODENAME, type='glob')
+            return all_nodenames[host]
+    except Exception as e:
+        error('could not get nodename: %s', e)
+        return ''
