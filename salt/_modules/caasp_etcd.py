9fc144e9989e142eeaea434e9c4e9ba7839c9676
Rafael Fernández López <ereslibre@ereslibre.es>
2018-09-06 13:09:21 +0200

    Use etcd api v2 on the 3.0 release branch.
    This commit can be reverted when we want to migrate to the etcd api v3.
    
    Fixes: bsc#1098433
    Fixes: bsc#1098064
    Fixes: bsc#1098161

--- salt/_modules/caasp_etcd.py
+++ salt/_modules/caasp_etcd.py
@@ -21,7 +21,7 @@ class NoEtcdServersException(Exception):
 
 
 def api_version():
-    return __salt__['pillar.get']('etcd_version', 'etcd3')
+    return __salt__['pillar.get']('etcd_version', 'etcd2')
 
 
 def _optimal_etcd_number(num_nodes):
