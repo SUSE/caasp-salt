7a8cd1870305d35452a3300a18a5ebb8458b27b1
Florian Bergmann <fbergmann@suse.de>
2018-11-15 11:05:42 +0100

    Fix bsc#1115236: Only add a new etcd member if no alias is already a member
    When adding a new member to etcd, it might happen that it is already
    part of the cluster using one of the aliases - when migrating from
    v2 to v3 it seems common that the default nodename changes.
    
    If this is the case it should not be added again with the new nodename,
    as one node can not have 2 etcd members.
    
    (cherry picked from commit 962a830f98a1300be23b63bfd78b4e3847eae2ab)

--- salt/_modules/caasp_etcd.py
+++ salt/_modules/caasp_etcd.py
@@ -503,6 +503,10 @@ def member_add(name=None, nodename=None, port=ETCD_PEER_PORT):
     '''
     this_id = name or __salt__['grains.get']('id')
     nodename_ = nodename or __salt__['caasp_net.get_nodename']()
+    aliases = __salt__['caasp_net.get_aliases'](nodename_)
+    if any([get_member_id(alias) for alias in aliases]):
+        # If this node is already registered in the cluster we pretend it was successfully added.
+        return True
     this_peer_url = 'https://{}:{}'.format(nodename_, port)
 
     debug('CaaS: adding etcd member %s', this_id)
