58fdf12acbb4c2efc1e42baffa5226d44b4d3f40
Miquel Sabaté Solà <msabate@suse.com>
2019-02-01 16:05:45 +0100

    caasp_filters: properly parse IP addresses
    As it appears, for Python 2 Salt is using a custom module for providing the
    ipaddress from Python 3. This implementation turned out to have
    bugs (e.g. accepting something70.domain.net as a valid IPv4 record). I've
    re-implemented the is_ip* functions so they are using socket.inet_pton instead,
    which should suffice in our use case.
    
    bsc#1123291

--- salt/_modules/caasp_filters.py
+++ salt/_modules/caasp_filters.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import
 
-from salt._compat import ipaddress
+import socket
+
+# TODO: in Python 3 there is an ipaddress module which works out of the box. In
+# fact, Salt is using this module when running in Python 3. For Python 2 Salt
+# is using a custom implementation, which is buggy on some checks. Thus,
+# whenever we jump into Python3, we should consider using either
+# salt._compat.ipaddress, or the module from Python 3.
 
 
 def __virtual__():
@@ -11,8 +17,6 @@ def is_ip(ip):
     '''
     Returns a bool telling if the passed IP is a valid IPv4 or IPv6 address.
     '''
-    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#is-ip)
-    #       once we Salt>2017.7.0
     return is_ipv4(ip) or is_ipv6(ip)
 
 
@@ -20,11 +24,10 @@ def is_ipv4(ip):
     '''
     Returns a bool telling if the value passed to it was a valid IPv4 address
     '''
-    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#is-ipv4)
-    #       once we Salt>2017.7.0
     try:
-        return ipaddress.ip_address(ip).version == 4
-    except ValueError:
+        socket.inet_pton(socket.AF_INET, ip)
+        return True
+    except socket.error:
         return False
 
 
@@ -32,15 +35,15 @@ def is_ipv6(ip):
     '''
     Returns a bool telling if the value passed to it was a valid IPv6 address
     '''
-    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#is-ipv6)
-    #       once we Salt>2017.7.0
     try:
-        return ipaddress.ip_address(ip).version == 6
-    except ValueError:
+        socket.inet_pton(socket.AF_INET6, ip)
+        return True
+    except socket.error:
         return False
 
 
 def get_max(seq):
-    # TODO: use the builtin filter (https://docs.saltstack.com/en/latest/topics/jinja/index.html#max)
+    # TODO: use the builtin filter
+    # (https://docs.saltstack.com/en/latest/topics/jinja/index.html#max)
     #       once we Salt>2017.7.0
     return max(seq)
